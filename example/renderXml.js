c6ef49633c45a29603cb81d77c275fd5a759cd24
marushkevych <marushkevych@gmail.com>
2015-01-22 14:33:17 -0500

    simplified example

--- example/renderXml.js
+++ example/renderXml.js
@@ -1,7 +1,5 @@
 
 React.render(
-    React.DOM.div({className: "xml"},
-        React.createElement(XmlDisplayComponent, {data: xml, expanded: true})
-    ),
+    React.createElement(XmlDisplayComponent, {data: xml, expanded: true}),
     document.getElementById('content')
 );
